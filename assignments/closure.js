14f275c9d8f3af2792c2d9ed4264052bfc63d564
Antoinette <create@aplusmedia.info>
2019-08-20 19:49:21 -0400

    complete closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -3,7 +3,11 @@
 // Keep it simple! Remember a closure is just a function
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
-
+const mom = "love";
+function myMom() {
+  return mom;
+}
+console.log(myMom());
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
